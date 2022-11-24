13fe984f6b437ce60dcdd124c664c5abc68d79c0
Abass <abass_gbolahan@yahoo.com>
2022-11-24 23:57:08 +0000

    page routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 
 function App() {
@@ -8,7 +12,15 @@ function App() {
     <Router>
        <div className="flex flex-col justify-between h-screen">
         <Navbar/>
-        <main>Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path ='/' element={<Home/>}></Route>
+            <Route path ='/about' element={<About/>}></Route>
+            <Route path ='/notfound' element={<NotFound/>}></Route>
+            <Route path ='/*' element={<NotFound/>}></Route>
+          </Routes>
+        </main>
+        <Footer/>
        </div>
     </Router>
   );
